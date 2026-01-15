d328e7b24d69ba5fbcb0c6d72bd80e90e4a8289b
Roy Chavez <roy.chavez@gmail.com>
2026-01-14 22:39:52 -0800

    Add /mrm route for static HTML page with image path fixes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     unoptimized: false,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/mrm',
+        destination: '/mrm/index.html',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
